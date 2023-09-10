53475be572819b9f6413571ec47a3866e494aca5
egorb <egor.baklach@gmail.com>
2023-09-10 13:13:52 +0500

    Some Refactoring

--- src/Exceptions/Traits/HeaderAndProxy.php
+++ src/Exceptions/Traits/HeaderAndProxy.php
@@ -2,8 +2,16 @@
 
 Trait HeaderAndProxy
 {
+    /** @var array */
+    private $additions;
+
     public function additions(array $additions): self
     {
-        [$this->attr['headers'], $this->attr['proxy']] = $additions; return $this;
+        $this->additions = $additions; return $this;
+    }
+
+    public function attr(): array
+    {
+        return [$this->url, null, $this->additions['headers'] ?? $this->headers ?? false, $this->additions['proxy'] ?? $this->proxy ?? false, $this->data, $this->options];
     }
 }
